b8349f225264aef488c091c571369c6085677a87
nm243012@163.com <nm243012@163.com>
2022-07-29 15:54:48 +0800

    ringbuffer

---
+++ RingBuffer.h
@@ -0,0 +1,31 @@
+//
+// Created by admin on 2022/7/29.
+//
+
+#pragma once
+
+#include <vector>
+
+//环形队列
+template<typename T, int Size = 8>
+struct RingBuffer {
+    constexpr RingBuffer() {
+
+    }
+
+    constexpr void push_back(const T &element) noexcept {
+
+    }
+
+    constexpr void push_back(T &&element) noexcept {
+
+    }
+
+    constexpr void pop() {
+
+    }
+
+    constexpr T &back() {
+
+    }
+};

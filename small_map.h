ee8c4a447df005291ec859ccf86d6e19c4b8487f
nm243012@163.com <nm243012@163.com>
2022-08-02 08:35:51 +0800

    add

--- small_map.h
+++ small_map.h
@@ -14,12 +14,31 @@ public:
     using key_type = Key;
     using mapped_type = Value;
 
+    auto begin() noexcept {
+        return _v.begin();
+    }
+
+    auto cbegin() noexcept {
+        return _v.cebgin();
+    }
+
+    auto end() noexcept {
+        return _v.end();
+    }
+
+
     std::size_t size() {
         return _v.size();
     }
 
     auto find(const Key &k) const noexcept {
         return std::find_if(_v.cbegin(), _v.cend(), [&](const value_type &v) {return v.first == k; });
+    }
+
+    auto find(const Key &k) noexcept {
+        return std::find_if(_v.cbegin(), _v.cend(), [&](const value_type &v) {
+            return v.first == k;
+        });
     }
 
     bool contain(const Key &k) const noexcept {
@@ -31,7 +50,13 @@ public:
     }
 
     Value& operator[](const Key& k) {
-
+        auto exisiting = find(k);
+        if (exisiting != _v.end()) {
+            return exisiting->second;
+        }
+        //如果没有的化make_pair一下
+        _v.push_back(std::make_pair(k, Value{}));
+        return _v.back().second;
     }
 private:
     std::vector<std::pair<Key, Value>> _v;
